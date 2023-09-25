813d5313ab330ae97954ab4d3050a2e93f09ff6e
DicksonBorges <dicksonborges19@gmail.com>
2023-09-25 22:00:47 +0530

    initial

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vaxi
+ Vaxi is a mobile application developed with Flutter, designed to offer comprehensive information about vaccines. This section provides users with valuable vaccine-related information and resources. Here, individuals have the option to create an account by providing their email address and setting up a password.
+
+
+# Features
+The application Vaxi offers three key features:
+Home Page: This page serves as an information hub, presenting users with valuable details about vaccines.
+Map Page: Users can access a map highlighting regions affected by ongoing disease outbreaks.
+Registration Page: This section enables users to register by providing their email addresses and creating passwords for their accounts.
+
+# Prerequisites
+1. Flutter SDK
+2. Android Studio or VS Code (with Flutter extension)
+3. Installation
+4. Clone the repository: git clone https://github.com/DicksonBorges/Vaxi.git
+5. Navigate to the project directory: cd vaxi
+6. Add your Google API key in the AndriodManifest.xml file in vac_app/andriod/app/src/main directory on line 35
+7. Run flutter pub get to install the required dependencies.
+8. Run the app: flutter run
+
+I recommend using android simulator.
---
+++ gsc/vac_app/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:vac_app/pages/home_page.dart';
+import 'package:vac_app/pages/login_page.dart';
+import 'firebase_options.dart';
+import 'helper/helper_function.dart';
+
+void main() async {
+  // to ensure that the widget is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSingnedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserloggedInStatus();
+  }
+
+  getUserloggedInStatus() async {
+    await HelperFunctions.getUserloggedInStatus().then((value) {
+      if (value != null) {
+        setState(() {
+          _isSingnedIn = value;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black87,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
